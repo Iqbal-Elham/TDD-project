f10003ab50d66a1f0dd7e3097b3ad6fa6d94ff33
johnie261 <njorogejohn261@gmail.com>
2023-05-25 22:56:52 +0300

    Fix linters

--- solver.rb
+++ solver.rb
@@ -1,28 +1,27 @@
 class Solver
-    def factorial(number)
-        if number.negative?
-            "Factorial is not possible to negative numbers!"
-        elsif number.zero?
-            1
-        else
-            number * factorial(number - 1)
-        end
+  def factorial(number)
+    if number.negative?
+      'Factorial is not possible to negative numbers!'
+    elsif number.zero?
+      1
+    else
+      number * factorial(number - 1)
     end
+  end
 
-    def reverse(string)
-        string.reverse
+  def reverse(string)
+    string.reverse
+  end
+
+  def fizzbuzz(num)
+    if (num % 3).zero? && (num % 5).zero?
+      'fizzbuzz'
+    elsif (num % 3).zero?
+      'fizz'
+    elsif (num % 5).zero?
+      'buzz'
+    else
+      num.to_s
     end
-
-    def fizzbuzz(num)
-        if num % 3 == 0 && num % 5 == 0
-            "fizzbuzz"
-        elsif num % 3 == 0
-            "fizz"
-        elsif num % 5 == 0
-            "buzz"
-        else
-            num.to_s
-        end
-    end
-
+  end
 end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,45 +1,45 @@
 require_relative '../solver'
 
 describe Solver do
-    solver = Solver.new 
+  solver = Solver.new
 
-    it "Should be the instance of the solver" do 
-        expect(solver).to be_instance_of(Solver)
-    end
+  it 'Should be the instance of the solver' do
+    expect(solver).to be_instance_of(Solver)
+  end
 
-    describe "Factorial" do
-        it "should return 1 if the number is 0" do
-            expect(solver.factorial(0)).to eql(1)
-        end
-        it "should return error message " do 
-            expect(solver.factorial(-2)).to eql("Factorial is not possible to negative numbers!")
-        end
-        it "should return 120, the factorial of number 5" do
-            expect(solver.factorial(5)).to eql(120)
-        end
+  describe 'Factorial' do
+    it 'should return 1 if the number is 0' do
+      expect(solver.factorial(0)).to eql(1)
     end
+    it 'should return error message ' do
+      expect(solver.factorial(-2)).to eql('Factorial is not possible to negative numbers!')
+    end
+    it 'should return 120, the factorial of number 5' do
+      expect(solver.factorial(5)).to eql(120)
+    end
+  end
 
-    describe "Reverse" do
-        it "Should reverse a string" do
-            expect(solver.reverse("khan")).to eql("nahk")
-        end
-        it "Should reverse a string" do
-            expect(solver.reverse("ok1ok")).to eql("ko1ko")
-        end
+  describe 'Reverse' do
+    it 'Should reverse a string' do
+      expect(solver.reverse('khan')).to eql('nahk')
     end
+    it 'Should reverse a string' do
+      expect(solver.reverse('ok1ok')).to eql('ko1ko')
+    end
+  end
 
-    describe "Fizzbuzz" do
-        it "Should return fizz when divisible by 3" do
-            expect(solver.fizzbuzz(3)).to eql("fizz")
-        end
-        it "Should return buzz when divisible by 5" do
-            expect(solver.fizzbuzz(5)).to eql("buzz")
-        end
-        it "Should return fizzbuzz when divisible by both 3 and 5" do
-            expect(solver.fizzbuzz(15)).to eql("fizzbuzz")
-        end
-        it "Should return string when not divisible by any" do
-            expect(solver.fizzbuzz(7)).to eql("7")
-        end
+  describe 'Fizzbuzz' do
+    it 'Should return fizz when divisible by 3' do
+      expect(solver.fizzbuzz(3)).to eql('fizz')
     end
+    it 'Should return buzz when divisible by 5' do
+      expect(solver.fizzbuzz(5)).to eql('buzz')
+    end
+    it 'Should return fizzbuzz when divisible by both 3 and 5' do
+      expect(solver.fizzbuzz(15)).to eql('fizzbuzz')
+    end
+    it 'Should return string when not divisible by any' do
+      expect(solver.fizzbuzz(7)).to eql('7')
+    end
+  end
 end
